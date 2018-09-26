a8d22225b66a74a73b6d7c2253df8ffd1f5129a4
adifalco <adifalco@nd.edu>
2018-09-26 14:32:03 -0400

    POC

--- crawler.py
+++ crawler.py
@@ -6,11 +6,11 @@ f = open("my_keys.txt","r+")
 lines = f.readlines()
 f.close()
 
-consumer_key=lines[0]
-consumer_secret=lines[1]
-access_token=lines[2]
-access_token_secret=lines[3]
-
+consumer_key=lines[0].strip('\n')
+consumer_secret=lines[1].strip('\n')
+access_token=lines[2].strip('\n')
+access_token_secret=lines[3].strip('\n')
+print(lines)
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -22,21 +22,21 @@ class FilteredStream(tweepy.StreamListener):
     def __init__(self,time_limit=60):
         self.start_time = time.time()
         self.limit = time_limit
-        os.system("touch tmp_results")
-        self.File = open("tmp_results.txt","r+")
+        self.File = open("data.txt","r+")
         super(FilteredStream,self).__init__()
 
     def on_status(self,status):
         if (time.time() - self.start_time) < self.limit:
-            if type(status.geo)!=type(None) and "job" not in status.text.lower():
-                print(status.text)
-                self.File.write("{0},{1},{2},{3},{4}\n".format(status.id_str,status.text.replace(',','^^^').replace('\n',"").replace('-&gt',""),status.geo['coordinates'][0],status.geo['coordinates'][1],status.created_at))
+            try:
+                self.File.write("{0},{1},{2}\n".format(status.id_str,status.text,status.created_at))
+            except UnicodeEncodeError:
+                pass
             return True
         else:
             self.File.close()
             return False
 
-os.system("touch tmp_results.txt")
+
 stream = tweepy.Stream(auth=api.auth,listener=FilteredStream(time_limit=10))
 stream.filter(locations=[-124,24,-66,49])
 print("done")
