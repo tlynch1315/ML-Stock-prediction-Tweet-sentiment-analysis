dc5f5d94308be2ac33f048e9b91c0a223c4cb39f
adifalco <adifalco@nd.edu>
2018-09-26 15:11:30 -0400

    Anthony: searching tweets based on copanies.txt

--- crawler.py
+++ crawler.py
@@ -17,7 +17,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-
+'''
 class FilteredStream(tweepy.StreamListener):
     def __init__(self,time_limit=60):
         self.start_time = time.time()
@@ -35,8 +35,22 @@ class FilteredStream(tweepy.StreamListener):
         else:
             self.File.close()
             return False
+'''
 
-
-stream = tweepy.Stream(auth=api.auth,listener=FilteredStream(time_limit=10))
-stream.filter(locations=[-124,24,-66,49])
-print("done")
+with open('companies.csv',"r+") as f:
+    lines = f.readlines()
+    for line in lines:
+        query = line.split(',')
+        print(query)
+        wf = open("data/{0}.json".format(query[0]),"r+")
+        wf.write('[')
+        for que in query:
+            data = [status for status in tweepy.Cursor(api.search,q=que).items(100)]
+            for status in data:
+                try:
+                    print(status.text)
+                    wf.write("{0}".format(status))
+                except UnicodeEncodeError:
+                    pass
+        wf.write(']')
+        wf.close()
