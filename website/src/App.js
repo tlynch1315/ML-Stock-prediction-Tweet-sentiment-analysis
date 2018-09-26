b34f84127144ee8307bef42d445d5a7eb41dc2f8
adifalco <adifalco@nd.edu>
2018-09-26 16:32:00 -0400

    Anthony: made POC frontend

---
+++ website/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Data Science Fall 2018</h1>
+          <p>Anthony DiFalco, Louis Theiry</p>
+          <p>Will Fritz, Tommy Lynch</p>
+        </header>
+        <p className="App-intro">
+          I'm thinking we have a listview here, and then when the user clicks we have some data&charts render
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
