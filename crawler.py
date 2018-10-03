4f1ac80448b2182b71051b44fbda58bd68ca49ba
adifalco <adifalco@nd.edu>
2018-10-02 21:54:47 -0400

    Anthony: waiting on finished csv to collect data

--- crawler.py
+++ crawler.py
@@ -1,56 +1,45 @@
 import time, tweepy, os
 import json
 
-
+DAY = "d1"
 f = open("my_keys.txt","r+")
-lines = f.readlines()
+lines_ = f.readlines()
 f.close()
 
-consumer_key=lines[0].strip('\n')
-consumer_secret=lines[1].strip('\n')
-access_token=lines[2].strip('\n')
-access_token_secret=lines[3].strip('\n')
-print(lines)
-
+consumer_key=lines_[0].strip('\n')
+consumer_secret=lines_[1].strip('\n')
+access_token=lines_[2].strip('\n')
+access_token_secret=lines_[3].strip('\n')
+print(lines_)
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-'''
-class FilteredStream(tweepy.StreamListener):
-    def __init__(self,time_limit=60):
-        self.start_time = time.time()
-        self.limit = time_limit
-        self.File = open("data.txt","r+")
-        super(FilteredStream,self).__init__()
 
-    def on_status(self,status):
-        if (time.time() - self.start_time) < self.limit:
-            try:
-                self.File.write("{0},{1},{2}\n".format(status.id_str,status.text,status.created_at))
-            except UnicodeEncodeError:
-                pass
-            return True
-        else:
-            self.File.close()
-            return False
-'''
+def touch(path):
+    with open(path,'a'):
+        os.utime(path,None)
 
 with open('companies.csv',"r+") as f:
     lines = f.readlines()
+    titles = ["name","ticker","ceo"]
     for line in lines:
         query = line.split(',')
-        print(query)
-        wf = open("data/{0}.json".format(query[0]),"r+")
-        wf.write('[')
-        for que in query:
-            data = [status for status in tweepy.Cursor(api.search,q=que).items(100)]
-            for status in data:
-                try:
-                    print(status.text)
-                    wf.write("{0}".format(status))
-                except UnicodeEncodeError:
-                    pass
-        wf.write(']')
-        wf.close()
+        for i in range(1,len(query)-2):
+            titles.append("key{0}".format(i))
+        for i in range(0,len(query)-1):
+            print(i)
+            path = "data/tweets/{0}/{1}-{2}.txt".format(DAY,query[0],titles[i])
+            exists = os.path.isfile(path)
+            if (exists==False):
+                touch(path)
+                wf = open(path,"r+")
+                que = query[i]
+                data = [status for status in tweepy.Cursor(api.search,q=que).items(100)]
+                for status in data:
+                    try:
+                        wf.write("{0}".format(status))
+                    except UnicodeEncodeError:
+                        pass
+                wf.close()
