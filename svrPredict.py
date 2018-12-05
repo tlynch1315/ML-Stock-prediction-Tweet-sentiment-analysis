6b033ad053d01b28624ec4a0b4e76519360534e1
Will Fritz <william.g.fritz@gmail.com>
2018-12-05 15:37:32 -0500

    added model training and prediction for svm

--- svrPredict.py
+++ svrPredict.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 import numpy as np 
-from sklearn.svm import SVR
+from sklearn import svm
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt 
 from get_dataframe import *
-from sklearn.cross_validation import train_test_split
 import math
-from sklearn.metrics import r2_score
+
 
 def trainSVR(X, Y, kernelChoice, C=1, degreeChoice=3, gammaChoice=1e-5):
     if kernelChoice == "linear":
@@ -19,117 +20,63 @@ def trainSVR(X, Y, kernelChoice, C=1, degreeChoice=3, gammaChoice=1e-5):
 
     return mySVR.fit(X, Y)
 
-def getCompanyData(dictionary, companyName):
-    df = dictionary[companyName]
-    df = df.drop(['change', 'open'],axis=1)
+def splitCompanyData(df, trainPercentage, labelName):
+    trainDays = np.random.rand(len(df)) < trainPercentage
 
-    grouped = df.groupby('date')
+    trainDf = df[trainDays]
+    xTrainDf = trainDf.drop(labelName, axis=1)
+    yTrainDf = trainDf[labelName]
 
-    totalDays = len(grouped)
-    trainingDays = math.floor(0.75*totalDays)
-    testingDays = totalDays - trainingDays
+    testDf = df[~trainDays]
+    xTestDf = testDf.drop(labelName, axis=1)
+    yTestDf = testDf[labelName]
 
-    dateList = [date for date in grouped]
-
-    trainList = dateList[:trainingDays]
-    testList = dateList[:testingDays]
-
-    trainDF = trainList.pop(0)[1]
-    testDF = testList.pop(0)[1]
-
-    for day in trainList:
-        trainDF = trainDF.append(day[1])
-
-    for day in testList:
-        testDF = testDF.append(day[1])
-
-    print(trainDF.head())
-    print(len(trainDF))
-    print(len(testDF))
-    print(len(grouped))
-    XTrainData = trainDF.drop(['value', 'date'], axis=1)
-    XTestData = testDF.drop(['value', 'date'],axis=1)
-
-    YTrainData = trainDF['value']
-    YTestData = testDF['value']
-
-    return XTrainData, XTestData, YTrainData, YTestData
+    return xTrainDf, yTrainDf, xTestDf, yTestDf
 
 
-# total days = 23
-df = pd.read_csv("./final.csv")
-tweet_dict = get_dataframe()
-
-companies = []
-
-with open('companies.csv', 'r') as f:
-    for line in f:
-        parts = line.split(",")
-        if parts[1] != 'SSNLF':
+def getCompaniesFromFile(filename):
+    companies = []
+    with open(filename, 'r') as f:
+        for line in f:
+            parts  = line.split(",")
             companies.append(parts[0])
-    f.close()
+        f.close()
+    return companies
+
+def trainSVC(x,y, kernelChoice, degreeChoice=3):
+    if kernelChoice == 'linear':
+        svcClassifier = svm.SVC(kernel=kernelChoice)
+    elif kernelChoice == 'poly':
+        svcClassifier = svm.SVC(kernel=kernelChoice, degree=degreeChoice)
+    else:
+        print("Error: Can't recognize your kernel choice of [{}] for SVC".format(kernelChoice))
+    y = y.astype('int')
+    svcClassifier.fit(x.values, y.values)
+    return svcClassifier
+
+if __name__ == "__main__":
+    companies = getCompaniesFromFile("companies.csv")
+    companyToDfDict = get_dataframe(companies)
+
+    trainPercentage = 0.85
+    classLblName = 'label' # 0 or 1 depending on whether a fall or rise from prev day
+    svcKernelChoice = 'linear'
+    for comp in companies:
+        xTrain, yTrain, xTest, yTest = splitCompanyData(companyToDfDict[comp], trainPercentage, classLblName)
+        svcClassifier = trainSVC(xTrain, yTrain, svcKernelChoice)
+        yTest = yTest.astype('int')
+        yPred = svcClassifier.predict(xTest.values)
+        print("\nClassification report for {}".format(comp))
+        print(classification_report(yTest.values, yPred))
+        print("Score for {}".format(comp))
+        print(svcClassifier.score(xTest.values, yTest.values))
+
+        # cross validation
+        svc = svm.SVC(kernel=svcKernelChoice)
+        scores = cross_val_score(svc, companyToDfDict[comp].drop(classLblName, axis=1).astype('int').values, companyToDfDict[comp][classLblName].astype('int').values, cv=3)
+        print("Accuracy for {}: {} (+/- {})".format(comp, scores.mean(), scores.std()*2))
 
 
-print(companies)
 
 
 
-XTrainTesla, XTestTesla, YTrainTesla, YTestTesla = getCompanyData(tweet_dict, 'Tesla')
-
-# make numbers a little more intuitive to look at
-YTrainTesla *= 1000
-YTestTesla *= 1000
-
-
-"""
-dfTesla = dfTesla.drop([ 'change', 'open'], axis=1)
-
-groupedTesla = 
-
-YTesla = dfTesla['value'].astype(float)
-XTesla['followers'] = XTesla['followers'].astype(float)
-XTesla['polarity'] = XTesla['polarity'].astype(float)
-XTesla['sentiment_confidence'] = XTesla['sentiment_confidence'].astype(float)
-
-XTesla = XTesla.groupby(date)
-
-# group by dates
-totalDays = len(XTesla)
-trainingDays = math.floor(0.75*totalDays)
-testingDays = totalDays - trainingDays
-for feature in XTesla:
-    XTesla.feature
-    print(days)
-
-XTrainTesla, XTestTesla, YTrainTesla, YTestTesla = train_test_split(XTesla.values, YTesla.values, random_state=42)
-"""
-kernel = "linear"
-svr_C = 100
-svr_degree = 2
-svr_gamma = 1e-5
-teslaSVR = trainSVR(XTrainTesla, YTrainTesla, "rbf", svr_C, svr_degree, svr_gamma)
-
-print(type(XTestTesla))
-YPredictTesla = []
-for index,row in XTestTesla.iterrows():
-    YPredictTesla.append(teslaSVR.predict(row.reshape(1,-1)))
-
-print(type(YPredictTesla[0]))
-
-plt.plot(range(len(YTestTesla)),YTestTesla)
-plt.plot(range(len(YTestTesla)),YPredictTesla)
-plt.show()
-
-"""
-print("5TH")
-print(dfTesla[dfTesla["date"] == "2018-10-05"]['change'][0])
-print(dfTesla[dfTesla["date"] == "2018-10-05"]['value'].sum())
-print("8TH")
-print(dfTesla[dfTesla["date"] == "2018-10-08"]['change'][0])
-print(dfTesla[dfTesla["date"] == "2018-10-08"]['value'].sum())
-
-print("value")
-print(dfTesla.groupby('date')['value'].sum())
-print("change")
-print(dfTesla.groupby('date')['change'].mean())
-"""
