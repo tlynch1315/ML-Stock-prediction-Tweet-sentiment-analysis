60f300ada1fc6cfdf9006cc590ca011d56441b61
Tommy <tlynch1315@gmail.com>
2018-12-07 18:44:04 -0500

    train on all data, add command line arguments

--- svrPredict.py
+++ svrPredict.py
@@ -1,11 +1,12 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from get_dataframe import *
 import math
+import argparse
 
 
 def trainSVR(X, Y, kernelChoice, C=1, degreeChoice=3, gammaChoice=1e-5):
@@ -13,7 +14,7 @@ def trainSVR(X, Y, kernelChoice, C=1, degreeChoice=3, gammaChoice=1e-5):
         mySVR = SVR(kernel = kernelChoice, C = 1)
     elif kernelChoice == "poly":
         mySVR = SVR(kernel = kernelChoice, C = 1, degree = degreeChoice)
-    elif kernelChoice == "rbf": 
+    elif kernelChoice == "rbf":
         mySVR = SVR(kernel = kernelChoice, C = 100, gamma = gammaChoice)
     else:
         print("ERROR in trainSVR, do not recognize the following svr kernel choice: {}", kernelChoice)
@@ -43,40 +44,46 @@ def getCompaniesFromFile(filename):
         f.close()
     return companies
 
-def trainSVC(x,y, kernelChoice, degreeChoice=3):
-    if kernelChoice == 'linear':
+def trainSVC(x,y, kernelChoice, degreeChoice=2, gamma='scale'):
+    '''if kernelChoice == 'linear':
         svcClassifier = svm.SVC(kernel=kernelChoice)
     elif kernelChoice == 'poly':
-        svcClassifier = svm.SVC(kernel=kernelChoice, degree=degreeChoice)
-    else:
-        print("Error: Can't recognize your kernel choice of [{}] for SVC".format(kernelChoice))
+        svcClassifier = svm.SVC(kernel=kernelChoice, degree=degreeChoice, gamma='scale')
+    else:'''
+    svcClassifier = svm.SVC(kernel=kernelChoice, degree=degreeChoice, gamma='scale')
+
+        #print("Error: Can't recognize your kernel choice of [{}] for SVC".format(kernelChoice))
     y = y.astype('int')
     svcClassifier.fit(x.values, y.values)
     return svcClassifier
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-k', dest='kernelChoice', type=str, default='linear')
+    parser.add_argument('-d', dest='degrees', type=int, default='2')
+    parser.add_argument('-g', dest='gamma', type=str, default='scale')
+    args = parser.parse_args()
     companies = getCompaniesFromFile("companies.csv")
     companyToDfDict = get_dataframe(companies)
 
     trainPercentage = 0.85
     classLblName = 'label' # 0 or 1 depending on whether a fall or rise from prev day
-    svcKernelChoice = 'linear'
+    svcKernelChoice = 'poly'
+    dfList = []
     for comp in companies:
-        xTrain, yTrain, xTest, yTest = splitCompanyData(companyToDfDict[comp], trainPercentage, classLblName)
-        svcClassifier = trainSVC(xTrain, yTrain, svcKernelChoice)
-        yTest = yTest.astype('int')
-        yPred = svcClassifier.predict(xTest.values)
-        print("\nClassification report for {}".format(comp))
-        print(classification_report(yTest.values, yPred))
-        print("Score for {}".format(comp))
-        print(svcClassifier.score(xTest.values, yTest.values))
+        dfList.append(companyToDfDict[comp])
+    dfTotal = pd.concat(dfList)
+    xTrain, yTrain, xTest, yTest = splitCompanyData(dfTotal, trainPercentage, classLblName)
+    svcClassifier = trainSVC(xTrain, yTrain, args.kernelChoice, degreeChoice=args.degrees, gamma=args.gamma)
+    yTest = yTest.astype('int')
+    yPred = svcClassifier.predict(xTest.values)
+    print("\nClassification report for All Data")
+    print(classification_report(yTest.values, yPred))
+    print("Score for {}".format("All Data"))
+    print(svcClassifier.score(xTest.values, yTest.values))
 
-        # cross validation
-        svc = svm.SVC(kernel=svcKernelChoice)
-        scores = cross_val_score(svc, companyToDfDict[comp].drop(classLblName, axis=1).astype('int').values, companyToDfDict[comp][classLblName].astype('int').values, cv=3)
-        print("Accuracy for {}: {} (+/- {})".format(comp, scores.mean(), scores.std()*2))
-
-
-
-
-
+    # cross validation
+    svc = svm.SVC(kernel=svcKernelChoice, degree=args.degrees, gamma=args.gamma)
+    scores = cross_val_score(svc, dfTotal.drop(classLblName, axis=1).astype('int').values, dfTotal[classLblName].astype('int').values, cv=3)
+    print("Accuracy for {}: {} (+/- {})".format("All Data", scores.mean(), scores.std()*2))
